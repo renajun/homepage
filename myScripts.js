bcf76ad1260afa015661ba09d6f0582e706e987d
renajun <256677@glenbrook225.org>
2024-11-07 09:31:20 -0600

    nov 7

---
+++ myScripts.js
@@ -0,0 +1,51 @@
+//POST-IT NOTE SAVE (JAVASCRIPT)
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ 
+ //POST-IT NOTE SHOW AND HIDE
+ var show = false;
+ function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+ //CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+   }
+   
